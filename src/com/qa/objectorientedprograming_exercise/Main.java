3f17ee4ebf53368cefbef3b7ec9975dc39771bf9
falgunipatel6789 <patelfalguni532@gmail.com>
2022-09-28 16:33:11 +0100

    method exercise

--- src/com/qa/objectorientedprograming_exercise/Main.java
+++ src/com/qa/objectorientedprograming_exercise/Main.java
@@ -3,17 +3,25 @@ package com.qa.objectorientedprograming_exercise;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product = new Product();
-		product.id = 1;
-		product.name = "computer";
-		product.price = 1200.0f;
+		Product product;
+		product = new Product(4, "webcam", 20.0, "asdf", (byte) 5, 10.0, true);
+		/*
+		 * product.id = 1;
+		 * 
+		 * product.name = "computer"; product.price = 1200.0f;
+		 * 
+		 * product.category = "class A"; product.rating = 5; product.isAvailable = true;
+		 * 
+		 * System.out.println(" product id is " + product.id);
+		 * System.out.println(" product name is " + product.name);
+		 * System.out.println(" product category is " + product.category);
+		 * 
+		 */
+		product.productdetail();
+		double productdiscprs = product.productdiscountprice();
+		System.out.println(" product discount price is " + productdiscprs);
 
-		product.category = "class A";
-		product.rating = 5;
-		product.isAvailable = true;
-		System.out.println(" product id is " + product.id);
-		System.out.println(" product name is " + product.name);
-		System.out.println(" product category is " + product.category);
+		System.out.println(" Product final price is " + product.productfinalprice());
 
 	}
 
--- src/com/qa/objectorientedprograming_exercise/Product.java
+++ src/com/qa/objectorientedprograming_exercise/Product.java
@@ -8,16 +8,48 @@ public class Product {
 	byte rating;
 	double discountPercentage;
 	boolean isAvailable;
-	Product(){
+
+	Product() {
+
 		id = 2;
-		name ="mouse";
-		price= 201.50f;
-		category="asdf";
+		name = "mouse";
+		price = 201.50f;
+		category = "asdf";
 		rating = 5;
-		isAvailable= true;
-		
-		
-				
+		isAvailable = true;
+
 	}
-	
+
+	public Product(int id, String name, double price, String category, byte rating, double discountPercentage,
+			boolean isAvailable) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.rating = rating;
+		this.discountPercentage = discountPercentage;
+		this.isAvailable = isAvailable;
+	}
+
+	void productdetail() {
+		System.out.println(" product id is " + id);
+		System.out.println(" product name is " + name);
+		System.out.println(" product category is " + category);
+
+	}
+
+	double productdiscountprice() {
+		return (price * discountPercentage / 100);
+
+	}
+
+	/*
+	 * Calculate the total salary after the increment
+	 */
+
+	double productfinalprice() {
+		return price - productdiscountprice();
+	}
+
 }
